8f366bdb3d834c2a95dedc570aa0c6476cc3bbc4
H-Bus <bheloise1@gmail.com>
2020-04-11 16:58:23 +0200

    correction bug

--- qtile.cpp
+++ qtile.cpp
@@ -10,11 +10,5 @@ Qtile::Qtile(QObject *parent) : QObject(parent)
 
 void Qtile::toStringCpp()
 {
-<<<<<<< HEAD
-    compteur++;
-    text = "Vous avez appuyé : "+QString::number(compteur,'f',0)+" fois sur le bouton";
-    emit changementCPP(text);
-=======
     qDebug() << "Hello World!";
->>>>>>> 527f5acfbe848ef762d29485a340a2c4e3e68f35
 }
